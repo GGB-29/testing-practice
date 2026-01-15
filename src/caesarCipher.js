dbaed490bdd31412b85a66c866016c5eaae6c788
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2026-01-15 18:24:56 +0000

    finish testing caesar cipher function

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,3 +1,20 @@
-export default function caesarCipher() {
-    
+export default function caesarCipher(string, shift) {
+    const normalizedShift = shift % 26;
+
+    return string
+            .split('')
+            .map(char => {
+                const code = char.charCodeAt(0);
+
+                if (code >= 97 && code <= 122) {
+                    return String.fromCharCode(((code - 97 + normalizedShift) % 26) + 97);
+                }
+                
+                if (code >= 65 && code <= 90) {
+                    return String.fromCharCode(((code - 65 + normalizedShift) % 26) + 65);
+                }
+                return char;
+                }
+            )
+            .join('');
 }
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -4,4 +4,32 @@ describe('caesar cipher function', () => {
         test('caesar cipher is a function', () => {
             expect(typeof caesarCipher).toBe('function');
         });
+
+        test('shifts lowercase letters', () => {
+            expect(caesarCipher('abc', 1)).toBe('bcd');
+        });
+
+        test('shifts uppercase letters', () => {
+            expect(caesarCipher('ABC', 1)).toBe('BCD')
+        });
+
+        test('wraps from z to a', () => {
+            expect(caesarCipher('xyz', 3)).toBe('abc');
+        });
+
+        test('wraps uppercase letters', () => {
+            expect(caesarCipher('XYZ', 3)).toBe('ABC');
+        });
+
+        test('preserves letter case', () => {
+            expect(caesarCipher('HeLLo', 1)).toBe('IfMMp');
+        });
+
+        test('preserves punctuation', () => {
+            expect(caesarCipher('a,b,c', 1)).toBe('b,c,d');
+        })
+
+        test('handles large shifts', () => {
+            expect(caesarCipher('abc', 27)).toBe('bcd');
+        });
 });

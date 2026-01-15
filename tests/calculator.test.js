0eb43c25d0d84cbef1060c2cc41574fb9464c4cf
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2026-01-15 16:41:25 +0000

    feat: add test one for calculator

---
+++ tests/calculator.test.js
@@ -0,0 +1,7 @@
+import calculator from '../src/calculator.js'
+
+describe('calculator object', () => {
+    test('calculator is an object', () => {
+        expect(typeof calculator).toBe('object');
+    });
+});

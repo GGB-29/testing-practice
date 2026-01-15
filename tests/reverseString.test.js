2c571e70990585db9cb3d71a8efcd7a371caa89d
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2026-01-15 16:19:33 +0000

    feat: add first test for reverseString

---
+++ tests/reverseString.test.js
@@ -0,0 +1,7 @@
+import reverseString from '../src/reverseString';
+
+describe('reverse string function', () => {
+    test('is a function', () => {
+        expect(typeof reverseString).toBe('function');
+    });
+});

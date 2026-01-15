3ce620e74fc54c90492b7833dea3a32d72bb1239
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2026-01-15 15:37:06 +0000

    add and pass third test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,9 +1,15 @@
 import capitalize from '../src/capitalize.js';
 
-test('capitalize is a function', () => {
-    expect(typeof capitalize).toBe('function');
-});
+describe('capitalize function', () => {
+    test('capitalize is a function', () => {
+        expect(typeof capitalize).toBe('function');
+    });
 
-test('capitalizes first letter of lowercase word', () => {
-    expect(capitalize("hello")).toBe("Hello");
+    test('capitalizes first letter of lowercase word', () => {
+        expect(capitalize("hello")).toBe("Hello");
+    });
+
+    test('does not change the rest of the string', () => {
+        expect(capitalize('javaScript')).toBe('JavaScript');
+    });
 });

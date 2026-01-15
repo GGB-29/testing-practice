573d5316f137603a5dd935a82a70b96de533515c
GGB-29 <216197362+GGB-29@users.noreply.github.com>
2026-01-15 15:30:20 +0000

    feat: add second test

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -3,3 +3,7 @@ import capitalize from '../src/capitalize.js';
 test('capitalize is a function', () => {
     expect(typeof capitalize).toBe('function');
 });
+
+test('capitalizes first letter of lowercase word', () => {
+    expect(capitalize("hello")).toBe("Hello");
+});
